a8ea8254429fb29beb63ab26f832e30167e5a803
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2022-01-04 19:31:23 -0300

    cria componente footer com hello world

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  const txt = 'HELLO WORLD footer!';
+  return (
+    <div className="footer-content">
+      {txt}
+    </div>
+  );
+}
+
+export default Footer;
