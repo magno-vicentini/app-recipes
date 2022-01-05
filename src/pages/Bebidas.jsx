fae88099ba743f5c1dcdadb1f8cc074e8b1fe2fa
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2022-01-05 17:34:08 -0300

    corrige pagina Bebidas

---
+++ src/pages/Bebidas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Bebidas() {
+  const txt = 'Bebidas';
+  return (
+    <div className="Bebidas-content">
+      {txt}
+    </div>
+  );
+}
+
+export default Bebidas;
