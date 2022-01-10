eceb558f2c73fa5fcb58728464f79a388de459a9
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2022-01-10 18:01:19 -0300

    chama useEffect na Buttons

--- src/components/FavoriteRecipes/FavoriteCard/Buttons/index.js
+++ src/components/FavoriteRecipes/FavoriteCard/Buttons/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import shareIcon from '../../../../images/shareIcon.svg';
 import blackHeartIcon from '../../../../images/blackHeartIcon.svg';
